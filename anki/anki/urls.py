064cff183220e17fbe23cfa37044e9be745bfb2e
helen <helen_gudkova@mail.ru>
2024-02-11 22:01:30 +0300

    django_14

--- anki/anki/urls.py
+++ anki/anki/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from cards.views import index
+from django.urls import path, include
+from cards import views
+#from cards.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index),
+    path('',views.index),
+    path('cards/', include('cards.urls')),
 ]
